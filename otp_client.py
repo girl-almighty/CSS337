f8c3c4c75e765df7111106e8dc822071db9610cf
Jon <jondycaico@gmail.com>
2018-11-04 19:01:07 -0800

    added troubleshooting file 'transformations.py'

--- otp_client.py
+++ otp_client.py
@@ -9,6 +9,20 @@ keys = []
 
 seed = '808670FF00FF08812'
 
+'''
+bin_seed = seed.encode()
+print(bin_seed)
+
+first_hash = hl.sha256(bin_seed)
+print(first_hash)
+
+hash_digest = first_hash.hexdigest()
+print(hash_digest)
+
+int_of_hash_digest = int(hash_digest, 16)
+print(int_of_hash_digest)
+'''
+
 first_hash = hl.sha256(seed.encode()).hexdigest()
 hashes.append(first_hash)
 
