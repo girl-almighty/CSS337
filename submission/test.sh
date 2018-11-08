78f4ae4f0016ccc130145a66b2fb0933a4bb0a08
girl-almighty <irisfvrl@uw.edu>
2018-11-07 21:26:31 -0800

    -

--- submission/test.sh
+++ submission/test.sh
@@ -4,7 +4,7 @@ echo "b
 10
 " | python3 otp_client.py
 
-echo "---Printing contents of Client output file---"
+echo; echo "---Printing contents of Client output file---"
 cat output.txt
 
 javac OTP_Server.java; java OTP_Server < output.txt
